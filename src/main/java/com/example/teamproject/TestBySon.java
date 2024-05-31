47731e6f32818d268ee06d78b53be9f6451ad1ea
hankyung <gksrud246@naver.com>
2024-05-31 12:43:55 +0900

    test

--- src/main/java/com/example/teamproject/TestBySon.java
+++ src/main/java/com/example/teamproject/TestBySon.java
@@ -2,6 +2,6 @@ package com.example.teamproject;
 
 public class TestBySon {
     public void writeSon() {
-        System.out.println("son:게시판 만드는중");
+        System.out.println("son:게시판 만드는중dddddd");
     }
 }
